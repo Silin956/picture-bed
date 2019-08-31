6f2157a67e7d3665315d19884ba1fddbe9df7ca3
原国超 <yuanguochao@yuanguochao.mzftech.cn>
2019-08-31 22:23:47 +0800

    delete unuse about vue

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="indexPage">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexView',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .indexPage{
+    
+  }
+</style>
